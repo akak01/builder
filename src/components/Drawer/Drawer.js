7e4b02925b7302e23e59f01a5f141eeddef24093
akak01 <syrgakovaakak010903@gmail.com>
2021-03-16 15:49:23 +0600

    Added files Logo and Nav

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,9 +1,12 @@
 import classes from "./Drawer.module.css";
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
 
 const Drawer = () => {
     return (
         <div className={classes.Drawer}>
-            Drawer
+            <Logo/>
+            <Nav/>
         </div>
     );
 }
