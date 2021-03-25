9b82ba6fe175f4256cb3d2999974440e97f1a39d
akak01 <syrgakovaakak010903@gmail.com>
2021-03-25 14:13:04 +0600

    Toolbar Had Changed code

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,7 +1,8 @@
-import classes from "./Toolbar.module.css";
+import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
 import Logo from "../../Logo/Logo";
 import Nav from "../../Nav/Nav";
-import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
+
+import classes from "./Toolbar.module.css";
 
 const Toolbar = ({ openDrawer }) => {
   return (
@@ -14,5 +15,5 @@ const Toolbar = ({ openDrawer }) => {
     </div>
   );
 }
- 
+
 export default Toolbar;
