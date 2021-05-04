86a1cc31bacd13e59e1e3d043c7fe4abef44b31b
akak01 <syrgakovaakak010903@gmail.com>
2021-05-04 14:16:57 +0600

    Add redux to react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import builderReduser from './store/builderReduser';
+
+const store = createStore(builderReduser);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/builderReduser.js
@@ -0,0 +1,7 @@
+const instalState = {};
+
+const builderReduser = (state = initialState, action) => {
+    return state;
+}
+
+export default builderReduser;
